1f6fc01a41c05de5810dbac1a162f4435a8489a9
Nicolas <nicolas.teixeira22@gmail.com>
2021-04-19 22:24:42 -0300

    Página sobre criada

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
+import Link from 'next/link';
+
 function Home(params) {
-    return <div>Home 2</div>
+    return (<div>
+        <h1>Home</h1>
+
+        <Link href="/sobre">
+        <a>Saiba mais</a>
+        </Link>
+        </div>)
 }
 
 export default Home
---
+++ pages/sobre.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+function sobre(params) {
+    return (<div>
+        <h1>sobre</h1>
+
+        <Link href="/">
+        <a>Acesse nossa página principal</a>
+        </Link>
+        </div>)
+}
+
+export default Home
